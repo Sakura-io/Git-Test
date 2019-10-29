222ca0af283d6fef5c146e196d17db409a4c6d84
root <2679882041@qq.com>
2019-10-29 11:04:14 +0800

    创建一个本地仓库

---
+++ untitled5/src/cn/baidu/Git.java
@@ -0,0 +1,9 @@
+package cn.baidu;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+    }
+}
