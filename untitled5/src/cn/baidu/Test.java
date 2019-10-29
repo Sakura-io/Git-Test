81b6fb718fe155105e5cccb5cebc2d988772f78f
root <2679882041@qq.com>
2019-10-29 11:48:14 +0800

    添加一个类

---
+++ untitled5/src/cn/baidu/Test.java
@@ -0,0 +1,12 @@
+package cn.baidu;
+
+public class Test {
+    public static void main(String[] args) {
+        int sum=0;
+        while (true){
+
+            System.out.println("我爱你:"+sum+"遍");
+            sum++;
+        }
+    }
+}
